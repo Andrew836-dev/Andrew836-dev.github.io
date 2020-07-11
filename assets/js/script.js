df62227703bb91ee6f1139ecd9d02bb433e9e17a
Andrew Gray <andrew.aj.gray@gmail.com>
2020-07-11 16:25:03 +0930

    Added jQuery scrolling, dynamically changing active navigation tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 $("document").ready(function () {
-    $(".tabs").on("click", "a", function () {
+    const tabs = $(".tabs").children();
+
+    $("#top").on("click", "a", function () {
         event.preventDefault();
-        $(this).siblings().removeClass("active");
+        tabs.removeClass("active");
         $(this).addClass("active");
-        
+        let id = $(this).attr("href");
+        $(window).scrollTop($(id).offset().top - 100);
+    });
+    
+    $(window).scroll(function () {
+        let scrollPosition = $(window).scrollTop();
+        tabs.removeClass("active");
+        if (scrollPosition > $("#contact").offset().top - 100) {
+            $("#contact-link").addClass("active");
+        } else if (scrollPosition > $("#about").offset().top - 100) {
+            $("#about-link").addClass("active");
+        } else if (scrollPosition > $("#portfolio").offset().top - 100) {
+            $("#portfolio-link").addClass("active");
+        }
     });
 });
