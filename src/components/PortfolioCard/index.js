dfb1c113360b0922f91b526e21329a44db177ca5
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-03 14:35:51 +0930

    added styling to portfolio cards

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import "./style.css";
 
 function PortfolioCard(props) {
-  const {title, homepageURL, image, repoURL, description} = props.repo;
+  const { title, homepageURL, image, repoURL, description } = props.repo;
   return (
-  <div>
-    <h4 className="repo-title">{title}</h4>
-    <p className="repo-text">{description}</p>
-    <img className="repo-image" src={image} alt={`${title} thumbnail`} />
-    <p><a className="button" href={homepageURL}>{homepageURL}</a></p>
-    <p><a className="button" href={repoURL}>{repoURL}</a></p>
-  </div>
+    <div className="five columns repo-card ">
+      <h4 className="repo-title">{title}</h4>
+      <p className="repo-text">{description}</p>
+      <div className="repo-image-container">
+        <img className="repo-image" src={image} alt={`${title} thumbnail`} />
+      </div>
+      <p>
+        <a className="button u-pull-left" href={homepageURL}>Live Site</a>
+        <a className="button u-pull-right" href={repoURL}>Repo</a>
+      </p>
+    </div>
   );
 }
 export default PortfolioCard;
