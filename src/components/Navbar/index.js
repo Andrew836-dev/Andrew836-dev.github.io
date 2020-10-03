3ba9ffa3c4232bf51886caf83b0c033083d6e47b
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-03 17:57:59 +0930

    styled navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './style.css'
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Navbar(props) {
   return (
@@ -13,13 +13,13 @@ function Navbar(props) {
           <div className="nine columns">
             <ul className="nav-list">
               <li className="nav-item">
-                <Link to="/portfolio" className="nav-link">Portfolio</Link>
+                <NavLink to="/portfolio" className="nav-link" activeClassName="nav-active">Portfolio</NavLink>
               </li>
               <li className="nav-item">
-                <Link to="/about" className="nav-link">About me</Link>
+                <NavLink to="/about" className="nav-link" activeClassName="nav-active">About me</NavLink>
               </li>
               <li className="nav-item">
-                <Link to="/contact" className="nav-link">Contact</Link>
+                <NavLink to="/contact" className="nav-link" activeClassName="nav-active">Contact</NavLink>
               </li>
             </ul>
           </div>
