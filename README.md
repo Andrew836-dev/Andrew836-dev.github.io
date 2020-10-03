f426524573db928d2a9d6ff0cd11a4e7bc3f3c9a
Andrew Gray <andrew.aj.gray@gmail.com>
2020-10-03 21:56:28 +0930

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# Personal Portfolio Site v4
-## [Live Site](https://andrew836-dev.github.io/index.html)
-This is my personal project portfolio, created 10/7/2020 for week 9 of my full stack bootcamp. I've created a few different versions in the past but wanted to try some alternative layouts.
-## Technology used
-* Chota CSS framework, similar to Bootstrap [link](https://jenil.github.io/chota/)
-* jQuery
---
+++ precache-manifest.3c21e60095152f43dadf20688f950a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f5d6b72a5b05998a758a9a4e18ef37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85267b2918c654ad07c8",
+    "url": "/static/css/main.02711b8f.chunk.css"
+  },
+  {
+    "revision": "bd01f3ca429975580f21",
+    "url": "/static/js/2.f511c9d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f511c9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85267b2918c654ad07c8",
+    "url": "/static/js/main.6527dfa5.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
